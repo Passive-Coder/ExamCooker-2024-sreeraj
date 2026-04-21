29efe0c8680a0c9ec7528686ae94cde59d9febab
theg1239 <thegreatestdropper@gmail.com>
2026-04-21 19:22:50 +0530

    Add Azure Pipelines deployment for App Service

--- startup.sh
+++ startup.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 set -eu
 
-if [ ! -x /node_modules/.bin/next ] && [ -f /home/site/wwwroot/node_modules.tar.gz ]; then
+cd /home/site/wwwroot
+
+if [ -d /home/site/wwwroot/node_modules ]; then
+  NODE_MODULES_PATH=/home/site/wwwroot/node_modules
+elif [ -d /node_modules ]; then
+  NODE_MODULES_PATH=/node_modules
+elif [ -f /home/site/wwwroot/node_modules.tar.gz ]; then
   rm -rf /node_modules
   mkdir -p /node_modules
   tar -xzf /home/site/wwwroot/node_modules.tar.gz -C /node_modules
+  NODE_MODULES_PATH=/node_modules
+else
+  echo "Could not find node_modules in /home/site/wwwroot or /node_modules." >&2
+  exit 1
 fi
 
-export NODE_PATH=/node_modules:/usr/local/lib/node_modules:${NODE_PATH:-}
-export PATH=/node_modules/.bin:${PATH}:/home/site/wwwroot
+export NODE_ENV="${NODE_ENV:-production}"
+export NODE_PATH="${NODE_MODULES_PATH}:/usr/local/lib/node_modules:${NODE_PATH:-}"
+export PATH="${NODE_MODULES_PATH}/.bin:${PATH}:/home/site/wwwroot"
 
-cd /home/site/wwwroot
-npm run start
+exec npm run start
